cd998598e2e13582bce41b3da1e50f056b71349f
AyalaBartal <ayalabartal@gmail.com>
2026-03-21 15:54:29 -0400

    Remove not used code from pe_dt_evaluate_args.py

--- src/specific/dt/evaluate/pe_dt_evaluate_args.py
+++ src/specific/dt/evaluate/pe_dt_evaluate_args.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 
 
@@ -16,18 +15,3 @@ class DtPeEvaluateArgs:
         self.out_json = self.feature_importance_csv = os.path.join(output_dir, 'dt_test_metrics.json')
         self.out_md = self.feature_importance_csv = os.path.join(output_dir, 'd_test_summary.md')
         self.out_png = self.feature_importance_csv = os.path.join(output_dir, 'dt_confusion_matrix.png')
-
-
-
-    @staticmethod
-    def get_train_args_from_cmd():
-        ap = argparse.ArgumentParser()
-        ap.add_argument('--data', required=True)
-        ap.add_argument('--label', required=True)
-        ap.add_argument('--model', required=True)
-        ap.add_argument('--threshold', type=float, default=0.5)
-        ap.add_argument('--out-json', default='reports/dt_test_metrics.json')
-        ap.add_argument('--out-md', default='reportst/d_test_summary.md')
-        ap.add_argument('--out-cm-png', default='reports/dt_confusion_matrix.png')
-        args = ap.parse_args()
-        return args
