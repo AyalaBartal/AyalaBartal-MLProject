8b53765729fdd1b0129686156e6637d363909aa2
AyalaBartal <ayalabartal@gmail.com>
2026-03-23 13:51:36 -0400

    Add TestPePdIntegration

--- src/common/validator/file_validator.py
+++ src/common/validator/file_validator.py
@@ -15,3 +15,15 @@ class FileValidator:
             raise FileNotFoundError('Not readable file: {}'.format(input_dir))
         if is_write and not os.access(input_dir, os.W_OK):
             raise FileNotFoundError('Not writeable file: {}'.format(input_dir))
+
+    @staticmethod
+    # Checks if the given path is an existing, readable, file and not a directory.
+    def validate_file(input_dir: str, is_read, is_write):
+        if not Path(input_dir).exists():
+            raise FileNotFoundError('Not found file: {}'.format(input_dir))
+        if os.path.isdir(input_dir):
+            raise FileNotFoundError('File is directory: {}'.format(input_dir))
+        if is_read and not os.access(input_dir, os.R_OK):
+            raise FileNotFoundError('Not readable file: {}'.format(input_dir))
+        if is_write and not os.access(input_dir, os.W_OK):
+            raise FileNotFoundError('Not writeable file: {}'.format(input_dir))
---
+++ tests/integration/dt/test_pe_dt_flow_state.py
@@ -0,0 +1,13 @@
+class TestPePdFlowState:
+    input_csv_file = None
+
+    output_root_dir_path = None
+
+    output_clean_csv_file = None
+    output_preprocess_csv_file = None
+
+    output_train_dir_path = None
+    output_model_file = None
+
+    output_evaluate_dir_path = None
+    dt_test_metrics_json_file = None
